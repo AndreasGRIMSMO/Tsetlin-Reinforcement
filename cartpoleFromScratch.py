377b45ad9d5ef7e80dcfc6b717148b2c439962e5
andreasGRIMSMO <andgrims@online.no>
2023-01-31 16:36:37 +0100

    Updated scratch file

--- cartpoleFromScratch.py
+++ cartpoleFromScratch.py
@@ -9,7 +9,7 @@ import gym
 X = np.load("statesCartPoleV5.npy")
 Y = np.loadtxt("actionsCartPoleV5.txt", dtype=int)
 print(type(Y))
-b = Binarizer(max_bits_per_feature=6)
+b = Binarizer(max_bits_per_feature=10)
 b.fit(X)
 clauses = 3000
 s = 3.9
@@ -24,18 +24,16 @@ steps = 10000
 explorationRate = 1.0
 
 for i in range(steps):
-    expCheck = False
     obsTemp = np.array([obs])
     state = b.transform(obsTemp)
     observation = 0
     if random.uniform(0, 1) <= explorationRate:
-        action = random.randint(0, 1)
-        obs, reward, done, info = eval_env.step(action)
-        expCheck = True
+        action1 = random.randint(0, 1)
+        action = np.array([action1])
+        obs, reward, done, info = eval_env.step(action[0])
     else:
         action = tm.predict(state)
         obs, reward, done, info = eval_env.step(action[0])
-        expCheck = False
     if i % 100 == 0 and i != 0 and explorationRate != 0:
         explorationRate -= 0.01
     elif i == 0:
@@ -44,7 +42,7 @@ for i in range(steps):
         tm.fit(obsInit, predInit, epochs=1)
 
     if done:
-        if action == 0:
+        if action[0] == 0:
             tm.fit(state, np.array([1]), epochs=1)
         else:
             tm.fit(state, np.array([0]), epochs=1)
@@ -56,10 +54,7 @@ for i in range(steps):
         # eval_env.render()
     else:
         score += 1
-        if expCheck:
-            tm.fit(state, np.array([action]), epochs=1)
-        else:
-            tm.fit(state, np.array([action[0]]), epochs=1)
+        tm.fit(state, np.array([action[0]]), epochs=1)
 
 print("Done with training")
 print("Largest reward: ", max(scores))
