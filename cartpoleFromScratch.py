74b3ff1a0b317a199bf154deef2a0f11ab15b24d
Didrik KD <didrik.kd@gmail.com>
2023-02-08 15:41:07 +0100

    updated FromScratchMethod

--- cartpoleFromScratch.py
+++ cartpoleFromScratch.py
@@ -9,9 +9,11 @@ import gym
 X = np.load("statesCartPoleV5.npy")
 Y = np.loadtxt("actionsCartPoleV5.txt", dtype=int)
 print(type(Y))
-b = Binarizer(max_bits_per_feature=10)
+b = Binarizer(max_bits_per_feature=6)
 b.fit(X)
-clauses = 3000
+del(X)
+del(Y)
+clauses = 500
 s = 3.9
 thresh = 0.4
 tm = MultiClassTsetlinMachine(clauses, clauses * thresh, s)
@@ -20,13 +22,25 @@ eval_env = gym.make("CartPole-v1")
 scores = []
 score = 0
 obs = eval_env.reset()
-steps = 10000
+steps = 10000000 #10000000 is too much
 explorationRate = 1.0
+rateChange = 0.01
+stepChange = steps * rateChange
+
+# initialize tsetlin
+obsTemp = np.array([obs])
+state = b.transform(obsTemp)
+obsInit = np.array([state, state])
+predInit = np.array([1, 0])
+tm.fit(obsInit, predInit, epochs=1)
+del(obsInit)
+del(predInit)
 
 for i in range(steps):
+    # Exploration and exploitation part
     obsTemp = np.array([obs])
+    firstAngle = abs(obs[2])
     state = b.transform(obsTemp)
-    observation = 0
     if random.uniform(0, 1) <= explorationRate:
         action1 = random.randint(0, 1)
         action = np.array([action1])
@@ -34,18 +48,28 @@ for i in range(steps):
     else:
         action = tm.predict(state)
         obs, reward, done, info = eval_env.step(action[0])
-    if i % 100 == 0 and i != 0 and explorationRate != 0:
-        explorationRate -= 0.01
-    elif i == 0:
-        obsInit = np.array([obs, obs])
-        predInit = np.array([1, 0])
-        tm.fit(obsInit, predInit, epochs=1)
-
-    if done:
+    if i % stepChange == 0 and i != 0 and explorationRate <= 0.1:
+        explorationRate -= rateChange
+    # elif i == 0:
+    #    obsInit = np.array([obs, obs])
+    #    predInit = np.array([1, 0])
+    #    tm.fit(obsInit, predInit, epochs=1)
+    # Learning
+    secondAngle = abs(obs[2])
+    # print(obs[2])
+    #if secondAngle >= 0.05 or firstAngle - secondAngle > 0:
+    if firstAngle - secondAngle > 0:
+        tm.fit(state, np.array([action[0]]), epochs=1)
+    else:
         if action[0] == 0:
             tm.fit(state, np.array([1]), epochs=1)
         else:
             tm.fit(state, np.array([0]), epochs=1)
+    if done:
+        # if action[0] == 0:
+        #    tm.fit(state, np.array([1]), epochs=1)
+        # else:
+        #    tm.fit(state, np.array([0]), epochs=1)
         obs = eval_env.reset()
         scores.append(score)
         # print(score)
@@ -54,23 +78,23 @@ for i in range(steps):
         # eval_env.render()
     else:
         score += 1
-        tm.fit(state, np.array([action[0]]), epochs=1)
+        # tm.fit(state, np.array([action[0]]), epochs=1)
 
 print("Done with training")
 print("Largest reward: ", max(scores))
 print("Starting Evaluation")
 scores = []
 score = 0
-for i in range(steps):
+obs = eval_env.reset()
+for i in range(10000):
     obsTemp = np.array([obs])
     state = b.transform(obsTemp)
-    observation = 0
     action = tm.predict(state)
     obs, reward, done, info = eval_env.step(action[0])
     if done:
         obs = eval_env.reset()
         scores.append(score)
-        #print(score)
+        # print(score)
         score = 0
         # print("DONE")
         # eval_env.render()
