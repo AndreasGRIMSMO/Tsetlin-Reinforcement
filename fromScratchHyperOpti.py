aaa3a66f375949da77546c42378b13d22a0d0722
Andreas Grimsmo <andrgr18@uia.no>
2023-02-21 14:02:24 +0100

    Update with extra loop for accuracy

--- fromScratchHyperOpti.py
+++ fromScratchHyperOpti.py
@@ -13,73 +13,76 @@ eval_env = gym.make("CartPole-v1")
 
 
 def objective(config):
+    means = []
     b = Binarizer(max_bits_per_feature=config["bits_per_feature"])
     b.fit(X)
-    tm = MultiClassTsetlinMachine(config["clauses"], config["clauses"] * config["thresh"], config["s"])
-    scores = []
-    score = 0
-    obs = eval_env.reset()
-    steps = 100000
-    explorationRate = 1.0
-    rateChange = 0.01
-    stepChange = steps * rateChange
-    minExp = 0.1
-    obsTemp = np.array([obs])
-    state = b.transform(obsTemp)
-    obsInit = np.array([state[0], state[0]])
-    predInit = np.array([1, 0])
-    tm.fit(obsInit, predInit, epochs=0)
-    del obsInit
-    del predInit
-    states = []
-    for i in range(steps):
+    for h in range(10):
+        tm = MultiClassTsetlinMachine(config["clauses"], config["clauses"] * config["thresh"], config["s"])
+        scores = []
+        score = 0
+        obs = eval_env.reset()
+        steps = 100000
+        explorationRate = 1.0
+        rateChange = 0.01
+        stepChange = steps * rateChange
+        minExp = 0.1
         obsTemp = np.array([obs])
-        firstAngle = abs(obs[2])
         state = b.transform(obsTemp)
-        if random.uniform(0, 1) <= explorationRate:
-            action1 = random.randint(0, 1)
-            action = np.array([action1])
-            obs, reward, done, info = eval_env.step(action[0])
-        else:
+        obsInit = np.array([state[0], state[0]])
+        predInit = np.array([1, 0])
+        tm.fit(obsInit, predInit, epochs=0)
+        del obsInit
+        del predInit
+        states = []
+        for i in range(steps):
+            obsTemp = np.array([obs])
+            firstAngle = abs(obs[2])
+            state = b.transform(obsTemp)
+            if random.uniform(0, 1) <= explorationRate:
+                action1 = random.randint(0, 1)
+                action = np.array([action1])
+                obs, reward, done, info = eval_env.step(action[0])
+            else:
+                action = tm.predict(state)
+                obs, reward, done, info = eval_env.step(action[0])
+            if i % stepChange == 0 and i != 0 and explorationRate <= 0.1:
+                explorationRate -= rateChange
+                if explorationRate < minExp:
+                    explorationRate = minExp
+            secondAngle = abs(obs[2])
+            states.append(state)
+            if firstAngle - secondAngle > 0:
+                tm.fit(state, np.array([action[0]]), epochs=1, incremental=True)
+            else:
+                if action[0] == 0:
+                    tm.fit(state, np.array([1]), epochs=1, incremental=True)
+                else:
+                    tm.fit(state, np.array([0]), epochs=1, incremental=True)
+
+            if done:
+                obs = eval_env.reset()
+                scores.append(score)
+                score = 0
+            else:
+                score += 1
+
+        scores = []
+        score = 0
+        obs = eval_env.reset()
+        for i in range(10000):
+            obsTemp = np.array([obs])
+            state = b.transform(obsTemp)
             action = tm.predict(state)
             obs, reward, done, info = eval_env.step(action[0])
-        if i % stepChange == 0 and i != 0 and explorationRate <= 0.1:
-            explorationRate -= rateChange
-            if explorationRate < minExp:
-                explorationRate = minExp
-        secondAngle = abs(obs[2])
-        states.append(state)
-        if firstAngle - secondAngle > 0:
-            tm.fit(state, np.array([action[0]]), epochs=1, incremental=True)
-        else:
-            if action[0] == 0:
-                tm.fit(state, np.array([1]), epochs=1, incremental=True)
+            if done:
+                obs = eval_env.reset()
+                scores.append(score)
+                score = 0
             else:
-                tm.fit(state, np.array([0]), epochs=1, incremental=True)
-
-        if done:
-            obs = eval_env.reset()
-            scores.append(score)
-            score = 0
-        else:
-            score += 1
-
-    scores = []
-    score = 0
-    obs = eval_env.reset()
-    for i in range(10000):
-        obsTemp = np.array([obs])
-        state = b.transform(obsTemp)
-        action = tm.predict(state)
-        obs, reward, done, info = eval_env.step(action[0])
-        if done:
-            obs = eval_env.reset()
-            scores.append(score)
-            score = 0
-        else:
-            score += 1
-    mean = statistics.mean(scores)
-    return mean
+                score += 1
+        mean = statistics.mean(scores)
+        means.append(mean)
+    return statistics.mean(means)
 
 
 search_space = {
